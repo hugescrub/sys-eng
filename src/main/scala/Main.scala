47506309a42d80d523b736d72a3e6c69103ab920
hugescrub <hexi9001@gmail.com>
2022-11-16 20:47:43 +0300

    package import & function call

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,13 @@
+import counter._
+import scala.io.StdIn
+
 object Main extends App {
-  val counter: Counter = new Counter
 
   print("Enter the value you want to count until: ")
   try {
-    val num: Int = readInt()
-    counter.countUntilNum(num)
+    val num: Int = StdIn.readInt()
+    writeToFile(compose, num)
   } catch {
-    case e: NumberFormatException => print("Given input is not a number.")
+    case e: NumberFormatException => println("Given input is not a number.")
   }
 }
